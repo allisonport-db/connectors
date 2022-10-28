bd247b752180330631f5cc06a3f8b2aa2fd2cfd0
Allison Portis <allison.portis@databricks.com>
2022-10-28 15:08:02 -0700

    fix imports

--- standalone/src/main/java/io/delta/standalone/actions/Metadata.java
+++ standalone/src/main/java/io/delta/standalone/actions/Metadata.java
@@ -16,7 +16,13 @@
 
 package io.delta.standalone.actions;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
