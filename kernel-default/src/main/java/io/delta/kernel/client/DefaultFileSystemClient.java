6285bc42df175a9184b8b574d28878416880e38b
Allison Portis <allison.portis@databricks.com>
2023-06-15 14:06:08 -1000

    FileSystem client APIs

--- kernel-default/src/main/java/io/delta/kernel/client/DefaultFileSystemClient.java
+++ kernel-default/src/main/java/io/delta/kernel/client/DefaultFileSystemClient.java
@@ -2,11 +2,15 @@ package io.delta.kernel.client;
 
 import io.delta.kernel.fs.FileStatus;
 import io.delta.kernel.utils.CloseableIterator;
+import io.delta.kernel.utils.Tuple2;
 import io.delta.storage.LocalLogStore;
 import io.delta.storage.LogStore;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
@@ -55,5 +59,38 @@ public class DefaultFileSystemClient
             @Override
             public void close() throws IOException { }
         };
+    }
+
+    private ByteArrayInputStream getStream(String filePath, Integer offset, Integer size) {
+        Path path = new Path(filePath);
+        try {
+            FileSystem fs = path.getFileSystem(hadoopConf);
+            DataInputStream stream = null;
+            try {
+                stream = fs.open(path);
+                stream.skipBytes(offset);
+                byte[] buff = new byte[size];
+                stream.readFully(buff);
+                return new ByteArrayInputStream(buff);
+            } catch (IOException ex) {
+                // TODO: exception handling here (iterator.next())
+                throw new RuntimeException(String.format(
+                        "IOException reading from file %s at offset %s size %s",
+                        filePath, offset, size), ex);
+            } finally {
+                if (stream != null) {
+                    stream.close();
+                }
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(String.format(
+                    "Could not resolve the FileSystem for path %s", filePath), ex);
+        }
+    }
+
+    @Override
+    public CloseableIterator<ByteArrayInputStream> readFiles(
+            CloseableIterator<Tuple2<String, Tuple2<Integer, Integer>>> iter) {
+         return iter.map(elem -> getStream(elem._1, elem._2._1, elem._2._2));
     }
 }
--- kernel/src/main/java/io/delta/kernel/client/FileSystemClient.java
+++ kernel/src/main/java/io/delta/kernel/client/FileSystemClient.java
@@ -2,7 +2,9 @@ package io.delta.kernel.client;
 
 import io.delta.kernel.fs.FileStatus;
 import io.delta.kernel.utils.CloseableIterator;
+import io.delta.kernel.utils.Tuple2;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -26,4 +28,11 @@ public interface FileSystemClient
      */
     CloseableIterator<FileStatus> listFrom(String filePath)
             throws FileNotFoundException;
+
+    /**
+     * TODO: solidify input type; need some combination of path, offset, size
+     */
+    CloseableIterator<ByteArrayInputStream> readFiles(
+            CloseableIterator<Tuple2<String, Tuple2<Integer, Integer>>> iter)
+            throws IOException;
 }
